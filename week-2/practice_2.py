9e3cf804a745565c16de09a8cd61e17012b76723
oghossgift <oghossgift@gmail.com>
2025-03-30 15:49:26 +0200

    (My third commit)

---
+++ week-2/practice_2.py
@@ -0,0 +1,11 @@
+str = input("Enter your state of origin: ")
+
+print("Your state of origin is: ", str)
+print("The first character is: ", str[0])
+print("The characters starting from 3 to 5: ", str[3..5])
+
+#string formatting
+name = "Ojo
+score = 79
+grade = "A"
+print(f"Name: {name; Score{score}; Grade{grade})
